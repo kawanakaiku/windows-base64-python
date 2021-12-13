6c4dd90a4a4ce07b8e132ca704f9fd0eaf8f79f6
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2021-12-13 19:59:14 +0900

    added KeyboardInterrupt hundler

--- base64.py
+++ base64.py
@@ -1,49 +1,63 @@
 import sys, os, base64
 
-decode = False
-file = ""
 
-for arg in sys.argv[1:]:
-   if arg == "-d":
-      decode = True
+def main():
 
-   elif file == "" and os.path.isfile(arg):
-      file = arg
+   decode = False
+   file = ""
 
-   else:
-      if not os.path.exists(arg):
-         sys.stdout.write("%s: no such file or directory.\n" % arg)
-         sys.exit(1)
+   for arg in sys.argv[1:]:
+      if arg == "-d":
+         decode = True
 
-      elif os.path.isdir(arg):
-         sys.stdout.write("%s: a directory.\n" % arg)
-         sys.exit(1)
-
-      elif file:
-         sys.stdout.write("%s: more than two files specified.\n" % arg)
-         sys.exit(1)
+      elif file == "" and os.path.isfile(arg):
+         file = arg
 
       else:
-         sys.stdout.write("%s: error.\n" % arg)
+         if not os.path.exists(arg):
+            sys.stdout.write("%s: no such file or directory.\n" % arg)
+            sys.exit(1)
+
+         elif os.path.isdir(arg):
+            sys.stdout.write("%s: a directory.\n" % arg)
+            sys.exit(1)
+
+         elif file:
+            sys.stdout.write("%s: more than two files specified.\n" % arg)
+            sys.exit(1)
+
+         else:
+            sys.stdout.write("%s: error.\n" % arg)
+            sys.exit(1)
+
+
+   if file == "":
+      input = sys.stdin.buffer.read()
+
+
+
+   else:
+      input = open(file, "rb").read()
+
+   if decode:
+      try:
+         output = base64.b64decode( input )
+
+      except base64.binascii.Error:
+         sys.stdout.write("binascii.Error: invalid input")
          sys.exit(1)
 
+   else:
+      output = base64.b64encode( input )
 
-if file == "":
-   input = sys.stdin.buffer.read()
-else:
-   input = open(file, "rb").read()
+   sys.stdout.buffer.write( output )
 
-if decode:
-   try:
-      output = base64.b64decode( input )
+   sys.exit(0)
 
-   except base64.binascii.Error:
-      sys.stdout.write("binascii.Error: invalid input")
-      sys.exit(1)
 
-else:
-   output = base64.b64encode( input )
+try:
+   main()
 
-sys.stdout.buffer.write( output )
-
-sys.exit(0)
+except KeyboardInterrupt:
+   sys.stdout.write("KeyboardInterrupt: exitting.")
+   sys.exit(1)
